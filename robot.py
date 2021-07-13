3b480f05076fced2cd37aed24a8ce328febdc84d
Tom Draper <tomjdraper1@gmail.com>
2021-07-13 15:06:03 +0100

    working avoidance

--- robot.py
+++ robot.py
@@ -46,24 +46,21 @@ class Robot:
         second = (self.x + (self.sensor_centre.length * self.direction.x), self.y + (self.sensor_centre.length * self.direction.y))
         
         xs, ys = interval_range(first, second)
-            
-        hit_coord = None    
+                    
         for x, y in zip(xs, ys):
+            print((int(round(x)), int(round(y))))
             if (int(round(x)), int(round(y))) in obstacles.coords:
-                hit_coord = (x, y)
-                break
-        
-        if hit_coord != None:
-            # Rotate left
-            r = random.randint(0, 1)
-            if r == 0:
-                print("Found obstacle, rotating left")
-                # Rotate slightly left
-                robot.direction.rotate(0.5)
-            else:
-                print("Found obstacle, rotating right")
-                # Rotate slightly right
-                robot.direction.rotate(-0.5)
+                print("FOUND OBSTACLE")
+                if random.randint(0, 1) == 0:
+                    # Suggest rotate slightly left
+                    return 0.7
+                else:
+                    # Suggest rotate slightly right
+                    return -0.7
+        for ob in obstacles.coords:
+            print(ob)
+                
+        return 0
     
     def checkLeftSensor(self, obstacles):
         # Get direction vector of 90 degrees left
@@ -74,16 +71,13 @@ class Robot:
         
         xs, ys = interval_range(first, second)
             
-        hit_coord = None    
         for x, y in zip(xs, ys):
             if (int(round(x)), int(round(y))) in obstacles.coords:
-                hit_coord = (x, y)
-                break
-        
-        if hit_coord != None:
-            # Rotate sligtly right
-            print("Found obstacle, rotating right")
-            robot.direction.rotate(-0.5)
+                print("FOUND OBSTACLE")
+                # Suggest rotate sligtly right
+                return -0.3
+
+        return 0
             
     def checkRightSensor(self, obstacles):
         # Get direction vector of 90 degrees left
@@ -94,23 +88,32 @@ class Robot:
         
         xs, ys = interval_range(first, second)
             
-        hit_coord = None    
         for x, y in zip(xs, ys):
             if (int(round(x)), int(round(y))) in obstacles.coords:
-                hit_coord = (x, y)
-                break
-        
-        if hit_coord != None:
-            # Rotate sligtly left
-            print("Found obstacle, rotating left")
-            robot.direction.rotate(0.5)
+                print("FOUND OBSTACLE")
+                # Suggest rotate slightly left
+                return 0.3
+                        
+        return 0
+    
+    def checkSensors(self, obstacles):
+        rotation = 0
+        rotation += self.checkCentreSensor(obstacles)
+        rotation += self.checkLeftSensor(obstacles)
+        rotation += self.checkRightSensor(obstacles)
+        return rotation
             
     def move(self, obstacles):
-        self.checkCentreSensor(obstacles)
-        self.checkLeftSensor(obstacles)
-        self.checkRightSensor(obstacles)
+        rotation = self.checkSensors(obstacles)
         
-        print("Moving", self.direction.x, self.direction.y)
+        # If no rotation suggested, random walk
+        if rotation == 0:
+            rotation = 0.05 * np.random.randn()
+        
+        print("ROTATING BY:", rotation)
+        robot.direction.rotate(rotation)
+
+        print("MOVING", self.direction.x, self.direction.y)
         
         self.x += self.direction.x
         self.y += self.direction.y
@@ -120,79 +123,71 @@ class Robot:
 class Obstacles:
     def __init__(self, plot_size):
         self.coords = set()
-        self.obstacles = [[(60, 60), (60, 70), (70, 70), (70, 60)]]
+        self.obstacles = [[(60, 60), (60, 70), (70, 70), (70, 60)], 
+                          [(10, 25), (25, 25), (25, 10)]]
         
         self.addOuterWalls(plot_size)
         
         for obstacle in self.obstacles:
             self.addObstacle(obstacle)
-        
-        print(self.coords)
-    
+            
     def addOuterWalls(self, plot_size):
         left = tuple((0, y) for y in range(plot_size+1))
         right = tuple((100, y) for y in range(plot_size+1))
         bottom = tuple((x, 0) for x in range(plot_size+1))
         top = tuple((x, 100) for x in range(plot_size+1))
         
-        self.coords.add(left)
-        self.coords.add(right)
-        self.coords.add(bottom)
-        self.coords.add(top)
+        self.coords.update(left)
+        self.coords.update(right)
+        self.coords.update(bottom)
+        self.coords.update(top)
     
     def addObstacle(self, obstacle):
         for i in range(len(obstacle)):
             if i != len(obstacle)-1:
                 first = obstacle[i]
                 second = obstacle[i+1]
-                
-                print(first, second)
-                
+                                
                 xs, ys = interval_range(first, second)
                 
                 for x, y in zip(xs, ys):
-                    print(x, y)
-                    self.coords.add((x, y))
+                    self.coords.add((int(round(x)), int(round(y))))
         
         # Finally, join the last and first element to complete the obstacle
         first = obstacle[-1]
         second = obstacle[0]
         
-        n_intervals = max(abs(first[0] - second[0]), abs(first[1] - second[1]))
-        if abs(first[0]-second[0]) != 0:
-            x_interval = abs(first[0]-second[0])/n_intervals
-        else:
-            x_interval = 1
-        if (abs(first[1]-second[1]) != 0):
-            y_interval = abs(first[1]-second[1])/n_intervals
-        else:
-            y_interval = 1
-        
-        xs = np.arange(start=first[0], stop=second[0], step=x_interval)
-        ys = np.arange(start=first[1], stop=second[1], step=y_interval)
+        xs, ys = interval_range(first, second)
         
         for x, y in zip(xs, ys):
-            print(x, y)
-            self.coords.add((x, y))
+            self.coords.add((int(round(x)), int(round(y))))
 
 
 def interval_range(first, second):
     n_intervals = max(abs(first[0] - second[0]), abs(first[1] - second[1]))
-    if abs(first[0]-second[0]) != 0:
-        x_interval = abs(first[0]-second[0])/n_intervals
+    
+    if first[0] != second[0]:
+        x_step = (second[0] - first[0])/n_intervals
     else:
-        x_interval = 1
-    if (abs(first[1]-second[1]) != 0):
-        y_interval = abs(first[1]-second[1])/n_intervals
+        x_step = 0
+        
+    if (first[1] != second[1]):
+        y_step = (second[1] - first[1])/n_intervals
     else:
-        y_interval = 1
+        y_step = 0
 
-    print(x_interval, y_interval)
+    if x_step == 0:
+        # Single value duplicated as many times as we need for ys
+        xs = [first[0]] * max(1, n_intervals)
+    else:
+        xs = np.arange(start=first[0], stop=second[0], step=x_step)
+        
+    if y_step == 0:
+        # Single value duplicated as many times as we need for xs 
+        ys = [first[1]] * max(1, n_intervals)
+    else:
+        ys = np.arange(start=first[1], stop=second[1], step=y_step)
 
-    xs = np.arange(start=first[0], stop=second[0], step=x_interval)
-    ys = np.arange(start=first[1], stop=second[1], step=y_interval)
-
-    print(xs, ys)
     return xs, ys
 
 
@@ -227,6 +222,8 @@ def animate(i):
 
     plt.xlim(0, plot_size)
     plt.ylim(0, plot_size)
+    plt.gca().set_aspect('equal', adjustable='box')
+
     
     robot.move(obstacles)
 
@@ -238,4 +235,5 @@ obstacles = Obstacles(plot_size)
 fig = plt.figure()
 
 ani = animation.FuncAnimation(fig, animate, interval=1000) 
+
 plt.show()
